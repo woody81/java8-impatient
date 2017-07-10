a926a02bedb95e050ea2bfc6441df67eb9802b12
woody81 <ondrej.lesak@gmail.com>
2017-07-10 23:20:43 +0200

    added Exercise 04 in chapter 2

---
+++ src/main/java/cz/woody81/java8/chapter02/Exercise04.java
@@ -0,0 +1,22 @@
+package cz.woody81.java8.chapter02;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by Ondra on 9. 7. 2017.
+ */
+public class Exercise04 {
+
+    @Test
+    public void perform() {
+        int[] numbers = {1, 4, 9, 16};
+        System.out.println(Arrays.toString(IntStream.range(1, 5).map(p -> p * p).toArray()));
+    }
+
+}
